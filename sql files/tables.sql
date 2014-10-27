47e56dcf86b5715a85ab77e0ebfd1d9931294b61
pranaydhondi <pranaydhondi@gmail.com>
2014-10-28 00:00:06 +0530

    creating tables

---
+++ sql files/tables.sql
@@ -0,0 +1,9 @@
+Create user {
+	profilefor varchar(50),
+	name varchar(50) not null,
+	gender varchar(50),
+	religion varchar(50),
+	mothertounge varchar(50),
+	emailid varchar(100) 
+	password 
+	}
